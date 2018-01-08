1eafe10c0269cde62abecb9fc13ecf6a686a1b56
Harald Lapp <harald@octris.org>
2018-01-08 23:35:45 +0100

    cleanup

--- libs/Validate.php
+++ libs/Validate.php
@@ -14,7 +14,6 @@ namespace Octris;
 /**
  * Validation base class.
  *
- * octdoc       c:core/validate
  * @copyright   copyright (c) 2010-2018 by Harald Lapp
  * @author      Harald Lapp <harald.lapp@gmail.com>
  */
@@ -25,26 +24,6 @@ class Validate
      */
     const T_OBJECT = 1;
     const T_ARRAY  = 2;
-
-    /**
-     * Available validation types.
-     */
-    const T_ALPHA     = '\Octris\Validate\Type\Alpha';
-    const T_ALPHANUM  = '\Octris\Validate\Type\Alphanum';
-    const T_BASE64    = '\Octris\Validate\Type\Base64';
-    const T_BOOL      = '\Octris\Validate\Type\Bool';
-    const T_DIGIT     = '\Octris\Validate\Type\Digit';
-    const T_EMAIL     = '\Octris\Validate\Type\Email';
-    const T_FILE      = '\Octris\Validate\Type\File';
-    const T_GENDER    = '\Octris\Validate\Type\Gender';
-    const T_MULTILINE = '\Octris\Validate\Type\Multiline';
-    const T_NUMERIC   = '\Octris\Validate\Type\Numeric';
-    const T_PATH      = '\Octris\Validate\Type\Path';
-    const T_PATTERN   = '\Octris\Validate\Type\Pattern';
-    const T_PRINTABLE = '\Octris\Validate\Type\Printable';
-    const T_PROJECT   = '\Octris\Validate\Type\Project';
-    const T_URL       = '\Octris\Validate\Type\Url';
-    const T_XDIGIT    = '\Octris\Validate\Type\Xdigit';
 
     /**
      * Validation types which are directly implemented in schema validator.
